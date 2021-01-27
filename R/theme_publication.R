174f89d4ea9cb617daeca033cddcb26f3a0e0261
Du Yang <duyang@bjmu.edu.cn>
2021-01-27 16:06:36 +0800

    add theme_publication

--- R/theme_publication.R
+++ R/theme_publication.R
@@ -4,6 +4,10 @@
 #' @param base_family font family, defaut 'sans'
 #' 
 #' @export
+#' @examples 
+#' library(ggplot2)
+#' ggplot(mtcars, aes(mpg, disp)) + geom_point() + theme_publication()
+#' @author Koundinya Desiraju (https://github.com/koundy/ggplot_theme_Publication)
 #' @import ggplot2
 #' @import ggthemes
 theme_publication <- function(base_size = 14, base_family = "sans") {
